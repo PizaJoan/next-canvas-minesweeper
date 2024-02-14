c8b1bf092193db0ec0a80deb704d2c2d32ce777a
Juan Piza Ferra <pizajoan97@gmail.com>
2024-02-14 19:01:38 +0100

    showing username when is logged in

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,32 @@
+'use server';
+
 import Link from 'next/link';
 import Script from 'next/script';
+import { cookies } from 'next/headers';
 
 import { GoogleLogin } from './google-login/google-login';
 
 // TODO: add is active condition and some styles to make it look cool
 export const Navbar = () => {
+  const username = cookies().get('username')?.value;
+
   return (
     <header className="flex flex-row justify-center gap-5 pt-10">
       <Link href="/">Home</Link>
       <Link href="/game">Game</Link>
       <Link href="/leaderboard">Leaderboard</Link>
-      <Script src="https://accounts.google.com/gsi/client" />
-      <GoogleLogin />
+      {username ? (
+        <div className="self-end">
+          <p>
+            Hello, <span className="font-semibold italic">{username}</span>
+          </p>
+        </div>
+      ) : (
+        <>
+          <Script src="https://accounts.google.com/gsi/client" />
+          <GoogleLogin />
+        </>
+      )}
     </header>
   );
 };
