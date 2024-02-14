d321d1631022a1b7b6d71892b6a16bef55123446
Juan Piza Ferra <pizajoan97@gmail.com>
2024-02-14 18:08:55 +0100

    changed cookie userId

--- app/api/user/init/route.ts
+++ app/api/user/init/route.ts
@@ -1,12 +1,12 @@
 import { cookies } from 'next/headers';
 
 export async function GET() {
-  const user = cookies().has('user');
+  const user = cookies().has('userId');
 
   if (!user) {
     const response = await fetch(`${process.env.API_URL}/user/init`);
 
-    cookies().set('user', await response.text());
+    cookies().set('userId', await response.text());
   }
 
   return Response.json({ message: 'loggedIn' });
